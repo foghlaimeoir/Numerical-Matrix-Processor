fce7523c2c66b26ea4bb1280820f24b613cb4070
CianLyons <cian.lyons@hotmail.com>
2021-06-09 19:28:48 +0100

    Added method to multiply a matrix by a constant

--- src/Matrix.java
+++ src/Matrix.java
@@ -10,34 +10,38 @@ class Matrix {
 
         int[][] firstMatrix = new int[firstRows][firstCols];
 
+
         for (int i = 0; i < firstRows; i++) {
             for (int j = 0; j < firstCols; j++) {
                 firstMatrix[i][j] = scanner.nextInt();
             }
         }
+        
+        int constant = scanner.nextInt();
 
-        int secondRows = scanner.nextInt();
-        int secondCols = scanner.nextInt();
 
-        if (firstRows != secondRows || firstCols != secondCols) {
-            System.out.println("ERROR");
-            return;
-        }
+//        int secondRows = scanner.nextInt();
+//        int secondCols = scanner.nextInt();
+//
+//        if (firstRows != secondRows || firstCols != secondCols) {
+//            System.out.println("ERROR");
+//            return;
+//        }
 
-        int[][] secondMatrix = new int[firstRows][firstCols];
+//        int[][] secondMatrix = new int[firstRows][firstCols];
 
-        for (int i = 0; i < firstRows; i++) {
-            for (int j = 0; j < firstCols; j++) {
-                secondMatrix[i][j] = scanner.nextInt();
-            }
-        }
+//        for (int i = 0; i < firstRows; i++) {
+//            for (int j = 0; j < firstCols; j++) {
+//                secondMatrix[i][j] = scanner.nextInt();
+//            }
+//        }
 
-        int[][] addedMatrix = addition(firstMatrix, secondMatrix);
+        int[][] multipliedMatrix = multiplyByConstant(constant, firstMatrix);
 
         for (int i = 0; i < firstRows; i++) {
             System.out.println("");
             for (int j = 0; j < firstCols; j++) {
-                System.out.print(addedMatrix[i][j] + " ");
+                System.out.print(multipliedMatrix[i][j] + " ");
             }
         }
     }
@@ -52,5 +56,14 @@ class Matrix {
         }
 
         return addedMatrix;
+    }
+
+    public static int[][] multiplyByConstant(int constant, int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = matrix[i][j] * constant;
+            }
+        }
+        return matrix;
     }
 }
